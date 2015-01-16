c0f42378979f220bec540fdae98a62b4b5a521cf
ismael <ismael.jimenez@gmail.com>
2015-01-15 22:20:21 -0600

    Initialize new repository for the game

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ frontend-nanodegree-arcade-game
 Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
 
 for self-checking their submission.
+
+
+The first part of the process was to identify the variables that i will need to move the player, and the enemies.
+
+from the video I could identify the player move between rows and columns, and the enemies just move in one direction but they have speed.
+
+So I proceed to add the speed parameters to the enemies.
+I found that it will be easier to create an object to keep the values of the stage, like the size, position of the player in the grid.
+
+Then I create the Stage object that will help me to set the values of the stage in terms of rows and columns. Also it will keep track of the player inside the grid, so it will be easier to calculate the collisions by just evaluating one of the coordinates.
+
--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+// The Stage where the player and enemies are.
+var Stage = function(  ){
+    // set the size in pixels of the stage
+    this.width = 500;
+    this.height = 500;
+
+    //set the rows porperties
+    this.rows = [];
+    numRows = 6;
+    var rowsbase = 75;
+
+    //set the cols porperties
+    this.cols = [];
+    numCols = 5;
+    var colsbase = 100;
+
+    // calculate and add the value to the stage rows
+    for (var i = 0; i < numRows; i++) {
+        this.rows[i] = rowsbase * i;
+    };
+
+    // calculate and add the value to the stage cols
+    for (var i = 0; i < numCols; i++) {
+        this.cols[i] = colsbase * i;
+    };
+
+    // player position in the stage
+    this.playerRow = 5;
+    this.playerColumn = 2;
+
+    // enemies position in the stage
+    this.enemies = [];
+
+}
+
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,9 +42,23 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 101;
-    this.y = 83;
+
+    this.generateValues();
 }
+
+Enemy.prototype.generateValues = function()
+{
+    // set one the position of the enemy randomly to one of
+    // the rows.
+    this.x = -30;
+
+    //get the random row
+    this.y = stage.rows[ Math.floor(Math.random() * 3) + 1 ];
+
+    //add the speed property
+    this.speed = Math.floor(Math.random() * 5) + 1;
+}
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -16,24 +66,30 @@ Enemy.prototype.update = function( dt ) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x+= this.speed;
+
+    // the width limit is reach?
+    if( this.x > stage.width )
+    {
+        this.generateValues();
+    }
 }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    console.log(this.x, this.y);
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage( Resources.get(this.sprite) , this.x , this.y );
 }
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function() {
+var Player = function( x , y ) {
     this.sprite = 'images/char-boy.png';
 
     //start position
-    this.x = 200;
-    this.y = 400;
+    this.x = x || 0;
+    this.y = y || 0;
 }
 
 Player.prototype.update = function( dt ){
@@ -46,15 +102,49 @@ Player.prototype.render = function( dt ){
 
 Player.prototype.handleInput = function( input ){
 
+    switch(input)
+    {
+        case 'left':
+            if( stage.playerColumn - 1 > -1 )
+            {
+                stage.playerColumn--;
+                player.x = stage.cols[ stage.playerColumn ];    
+            }
+        break;
+        case 'right':
+            if( stage.playerColumn + 1 < stage.cols.length )
+            {
+                stage.playerColumn++;
+                player.x = stage.cols[ stage.playerColumn ];
+            }
+        break;
+        case 'up':
+            if( stage.playerRow - 1 > -1 )
+            {
+                stage.playerRow--;
+                player.y = stage.rows[ stage.playerRow ];    
+            }
+        break;
+        case 'down':
+            if(stage.playerRow + 1  <  stage.rows.length )
+            {
+                stage.playerRow++;
+                player.y = stage.rows[ stage.playerRow ];    
+            }
+        break;
+    }
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var allEnemies = [new Enemy(),new Enemy(),new Enemy()];
+var stage = new Stage(  );
 
-var player = new Player();
+var allEnemies = [ new Enemy(), new Enemy(), new Enemy() ];
+
+var player = new Player( stage.cols[ stage.playerColumn ] ,
+                        stage.rows[ stage.playerRow ] );
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -65,6 +155,7 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
+
     console.log( e.keyCode );
     player.handleInput(allowedKeys[e.keyCode]);
 });
